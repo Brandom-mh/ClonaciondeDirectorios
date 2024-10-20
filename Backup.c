9e5101a6f3838b4bb3fcb4d5b3358d6c7c10a449
Brandom-mh <brandom_mh@comunidad.unam.mx>
2024-10-20 14:45:45 -0600

    Update Backup.c

--- Backup.c
+++ Backup.c
@@ -22,14 +22,14 @@ int main(int argc, char *argv[])
 
     char ruta_lista[200];
 
-    // Verificar que se pasen las rutas de origen y destino
+
     if (argc != 3)
     {
         fprintf(stderr, "Uso: %s /ruta/origen /ruta/destino\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Verificacion del pipe
+ 
     if (pipe(pfd) == -1)
     {
         perror("pipe");
@@ -42,11 +42,11 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Rutas de origen y destino
+    
     char *origen = argv[1];
     char *destino = argv[2];
 
-    // Crear el proceso hijo
+    
     pid = fork();
 
     if (pid == -1)
@@ -56,20 +56,18 @@ int main(int argc, char *argv[])
     }
 
     if (pid == 0)
-    { // Proceso hijo
+    { 
 
-        close(pfd[1]);  // Cerrar el extremo de escritura del pipe
-        close(pfd2[0]); // cerrar lectura del pipe2
-        // Leer el mensaje del padre
+        close(pfd[1]); 
+        close(pfd2[0]); 
+        
         printf("\n\nHIJO(pid=%d): esperando mensaje de mi padre...", getpid());
         read(pfd[0], bufferHijo, MAX);
-        // close(pfd[0]);
         printf("\nHIJO(pid=%d): Instrucción de mi padre: %s\n", getpid(), bufferHijo);
         fflush(stdout);
 
         write(pfd2[1], mensajehijo, MAX);
-        // close(pfd2[1]);
-
+    
         read(pfd[0], bufferHijo, MAX);
 
         int numArch;
@@ -85,29 +83,29 @@ int main(int argc, char *argv[])
             printf("\n\tHijo(pid=%d),respaldando el archivo %s   pendientes:%d/%d\n", getpid(), bufferHijo, (numArch - resta), numArch);
             fflush(stdout);
             resta++;
+            
         }
 
         write(pfd2[1], "Adios Padre, termine el respaldo...", 36);
-
-        // Ejecutar el comando "cp -r /ruta/origen /ruta/destino"
         snprintf(comando, sizeof(comando), "mv %s %s", origen, destino);
         system(comando);
+
+    
     }
     else
-    { // Proceso padre
+    { 
 
-        // Cerrar el extremo de lectura del pipe
         close(pfd[0]);
-        close(pfd2[1]); // cerramos escritura del pipe2
+        close(pfd2[1]); 
 
         printf("\nPADRE(pid=%d): generando LISTA DE ARCHIVOS A RESPALDAR", getpid());
         printf("\nPADRE(pid=%d): borrando respaldo viejo...\n", getpid());
 
-        // Revisando si el directorio de destino existe
+     
         snprintf(comando, sizeof(comando), "test -d %s", destino);
         if (system(comando) != 0)
         {
-            // Si el directorio de destino no existe, lo crea
+           
             printf("\nEl directorio de destino no existe. Creando...\n");
             snprintf(comando, sizeof(comando), "mkdir -p %s", destino);
             if (system(comando) == -1)
@@ -121,7 +119,7 @@ int main(int argc, char *argv[])
         {
             printf("El directorio de destino ya existe: %s\n", destino);
         }
-        // Crear el archivo lista.txt en el directorio de destino
+        
         snprintf(ruta_lista, sizeof(ruta_lista), "%s/Respaldo.txt", destino);
         archivo = fopen(ruta_lista, "w");
 
@@ -134,17 +132,14 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        // Escribir los nombres de archivos y directorios del directorio de origen en lista.txt
         DIR *dir;
         struct dirent *ent;        
         int total_archivos = 0;
 
         if ((dir = opendir(origen)) != NULL)
         {
-            // Recorrer los archivos y directorios en el origen
             while ((ent = readdir(dir)) != NULL)
             {
-                // Ignorar "." y ".."
                 if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0)
                 {
                     printf("\nremoved '%s'\n", ent->d_name);
@@ -166,11 +161,8 @@ int main(int argc, char *argv[])
 
         numero = total_archivos;
 
-        // Escribir el número total de archivos y directorios en lista.txt
-        fprintf(archivo, "Total de archivos y directorios: %d\n", total_archivos);
+        fprintf(archivo, "%d ARCHIVOS RESPALDO \n", total_archivos);
         fclose(archivo);
-
-        // Enviar el mensaje al hijo para que comience el respaldo
         write(pfd[1], mensaje, MAX);
 
         read(pfd2[0], bufferPadre, MAX);
@@ -183,10 +175,8 @@ int main(int argc, char *argv[])
 
         if ((dir = opendir(origen)) != NULL)
         {
-            // Recorrer los archivos y directorios en el origen
             while ((ent = readdir(dir)) != NULL)
             {
-                // Ignorar "." y ".."
                 if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0)
                 {
                     printf("\n(PADRE-->%s)\n", ent->d_name);
@@ -223,8 +213,6 @@ int main(int argc, char *argv[])
         fclose(archivo);
         printf("\n======================================================================\n");
         printf("\nTermino el proceso padre...\n");
-        // Esperar la finalización del hijo
-
         wait(NULL);
     }
 
